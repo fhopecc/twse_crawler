e336dfd1874df98d777e61671ade5c5c2442e4fc
fhopecc <fhopecc@gmail.com>
2026-06-03 17:08:41 +0800

    完成蒐整財務資訊

--- twse_crawler/股票評級.py
+++ twse_crawler/股票評級.py
@@ -262,8 +262,7 @@ if __name__ == '__main__':
     import logging
     logging.getLogger('googleapiclient').setLevel(logging.CRITICAL)
     logging.basicConfig(level=logging.INFO)
-    # cache.clear()
-    蒐整財務資訊()
-    # zhongwen.快取.停止快取=True
-    # 顯示股票評級彙總表(0.05)
+    cache.clear()
+    zhongwen.快取.停止快取=True
+    顯示股票評級彙總表(0.05)
     # 列出函數執行時間表()
