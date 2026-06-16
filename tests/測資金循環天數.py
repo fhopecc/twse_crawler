6c6866414c6cb268dae9f2c5a90b306109005d32
fhopecc <fhopecc@gmail.com>
2026-06-16 17:15:24 +0800

    增加 CCC

---
+++ tests/測資金循環天數.py
@@ -0,0 +1,18 @@
+from unittest.mock import patch
+import unittest
+
+class Test(unittest.TestCase):
+    '依方法名稱字母順序測試'
+    def test(self):
+        from zhongwen.表 import 表示
+        from twse_crawler.資金循環天數 import 分析資金循環
+        r = 分析資金循環('鈊象') 
+        表示(r)
+ 
+if __name__ == '__main__':
+    import logging
+    logging.basicConfig(level=logging.INFO)
+    logging.getLogger('googleclient').setLevel(logging.CRITICAL)
+    logging.getLogger('matplotlib').setLevel(logging.CRITICAL)
+    logging.getLogger('faker').setLevel(logging.CRITICAL)
+    unittest.main()
