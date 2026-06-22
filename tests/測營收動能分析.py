f7826eecdd37470f9dc7c4a1bd9077fe7eb6bed7
fhopecc <fhopecc@gmail.com>
2026-06-22 21:05:29 +0800

    營收動能

---
+++ tests/測營收動能分析.py
@@ -0,0 +1,21 @@
+import unittest
+
+class Test(unittest.TestCase):
+    '依方法名稱字母順序測試'
+    def test(self):
+        from twse_crawler.營收動能分析 import 分析營收動能
+        from zhongwen.表 import 表示
+        df = 分析營收動能('一零四')
+        表示(df)
+        
+ 
+if __name__ == '__main__':
+    import logging
+    logging.basicConfig(level=logging.INFO)
+    logging.getLogger('googleclient').setLevel(logging.CRITICAL)
+    logging.getLogger('matplotlib').setLevel(logging.CRITICAL)
+    logging.getLogger('faker').setLevel(logging.CRITICAL)
+    unittest.main()
+    suite = unittest.TestSuite()
+    suite.addTest(Test('test'))  # 指定測試
+    unittest.TextTestRunner().run(suite)
